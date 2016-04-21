199b3c1bb57a86d68df7b71beb6a1d11c1efd58b
Mark van Kleef <mkleef@avans.nl>
2016-04-21 20:46:55 +0200

    Preparation of getting seasons from user

--- model/season.js
+++ model/season.js
@@ -3,6 +3,14 @@ var Schema = mongoose.Schema;
 
 var seasonSchema = new Schema({
 	name : {type: String, required: true},
+	colors: [{
+		color: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'SeasonColor'
+		},
+	}]
 });
+
+seasonSchema.path('name').required(true, 'Name must be set');
 
 var Season	= mongoose.model('Season', seasonSchema);
---
+++ model/seasoncolor.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var seasonColorSchema = new Schema({
+	_season : {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Season'
+	},
+	_color : {
+		type: Schema.Types.ObjectId,
+		ref: 'Color'
+	}
+});
+
+var SeasonColor	= mongoose.model('SeasonColor', seasonColorSchema);
